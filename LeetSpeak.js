534ebb36c41f665ee60826aeebf1653ce16cad47
Arthur Oganezov <arthur.oganezov@gmail.com>
2016-04-16 15:07:40 -0500

    l33t added

---
+++ LeetSpeak.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    var leetDict = {
+        "cool": "kewl",
+        "ate": '8',
+        "be right back": "brb",
+        "by the way": "btw",
+        "cks": 'x',
+        "are": 'r',
+        'a': '4',
+        'c': '(',
+        'd': '|)',
+        'e': '3',
+        'f': "ph",
+        'g': '9',
+        'h': '|-|',
+        'i': '!',
+        'j': '_|',
+        'k': '|<',
+        'l': '|_',
+        'm': "|\\/|",
+        'n': "/\\/",
+        'o': '0',
+        's': '$',
+        't': '+',
+        'u': 'µ',
+        'v': "\\/",
+        'w': '|/\\|',
+        'X': '><',
+        'y': '¥',
+        'z': '2'
+    };
+
+
+    //noinspection JSUnresolvedFunction
+    var string = 'This is a test! Hello, World!'.toLowerCase();
+    var tag = $("body");
+    var res = '';
+        for (var i = 0; i < tag.length; ++i) {
+
+            for (var j = 0; j < tag[i].innerText.length; ++j) {
+
+                if (typeof leetDict[tag[i].innerText[j].toLowerCase()] !== 'undefined') {
+                    res += leetDict[tag[i].innerText[j].toLowerCase()];
+                    console.log(leetDict[tag[i].innerText[j].toLowerCase()]);
+                } else {
+                    res += tag[i].innerText[j].toLowerCase();
+                    //console.log();
+                }
+
+
+
+            }
+            tag[i].innerText = res;
+            res = '';
+        }
+
+});
