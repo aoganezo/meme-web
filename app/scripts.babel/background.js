c2cd851529f6b1cfd489213ccfe445de10d27802
David Gorelik <dgore7@gmail.com>
2016-04-16 23:48:01 -0500

    hi mom!

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -10,21 +10,31 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 
 
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
-
 console.log('\'Allo \'Allo! Event Page for Browser Action');
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  alert('win');
-  chrome.tabs.insertCSS(
+    alert('win');
+    chrome.tabs.insertCSS(
       {
         file : 'styles/main.css'
       });
-  if (typeof $ !== 'undefined') {
+
     chrome.tabs.executeScript({
-      file : "jquery.min.js"
+        file : 'jquery.min.js',
+        code : 'alert(\'test\');'
+
     });
-  }
+
 });
+function updateCursor(){
+    var count = 0;
+    var urlStart = 'chrome-extension://bficihnlmjjnokaiocbjbpnjohgoaagj/Cursor/125cd106f4c94fe3d110fda6ed40ddbd-';
+    var urlEnd = '.cur';
+
+    count = ((count + 1) % 12);
+    console.log($);
+    $('html').cursor = '(chrome-extension://bficihnlmjjnokaiocbjbpnjohgoaagj/Cursor/125cd106f4c94fe3d110fda6ed40ddbd-0.cur)';
+
+}
 
 
