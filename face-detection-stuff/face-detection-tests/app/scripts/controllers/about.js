fc0785fff41277d4070763da8af2d8d29e1552e7
August Karlstedt <August.Karlstedt@Gmail.com>
2016-04-16 23:42:56 -0500

    * face detection stuffs

---
+++ face-detection-stuff/face-detection-tests/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name faceDetectionTestsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the faceDetectionTestsApp
+ */
+angular.module('faceDetectionTestsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
