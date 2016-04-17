1fc029471efb5b2c774b3893e04df94b27e885f9
David Gorelik <dgore7@gmail.com>
2016-04-16 20:13:45 -0500

    connected things

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -4,6 +4,27 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
+chrome.browserAction.onClicked.addListener(function(tab) {
+
+});
+
+
+
 chrome.browserAction.setBadgeText({text: '\'Allo'});
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  alert('win');
+  chrome.tabs.insertCSS(
+      {
+        file : 'styles/main.css'
+      });
+  if (typeof $ !== 'undefined') {
+    chrome.tabs.executeScript({
+      file : "jquery.min.js"
+    });
+  }
+});
+
+
